4fe44bad8216c01c45dd7dd80ee0da290ce7e30b
Barbara Bruno <barbara.bruno@epfl.ch>
2021-09-01 18:21:26 +0200

    added decisions on assessment

--- irecheck/scripts/decisionMaker.py
+++ irecheck/scripts/decisionMaker.py
@@ -14,6 +14,7 @@ class DecisionMaker():
         rospy.Subscriber('dynamicomsg', String, self.dynamicoCallback)
         self.pubFSMMsg = rospy.Publisher('autodecisions', String, queue_size=10)
         self.pubBehMsg = rospy.Publisher('/irecheck/button_name', String, queue_size=1)
+        self.pubSayMsg = rospy.Publisher('/qt_robot/speech/say', String, queue_size=1)
 
         # keep python from exiting until this node is stopped
         rospy.spin()
@@ -27,20 +28,47 @@ class DecisionMaker():
         # DEBUG ONLY
         print(df)
 
-        # implement a simple logic to determine what to suggest next:
-        # if the score in the current game is above 50 --> congratulations and move to GOODBYE
-        # else --> encouragement and try again
-        if (df.at[0, 'score'] > 50 ):
-            msg = 'bravo'
+        # implement a simple logic to determine what to suggest next
+        # if ASSESSMENT --> suggest the activity associated with the lowest score, move to ACTIVITY state
+        if (df.at[0, 'type'] == 'assessment'):
+            if (min([df.at[0, 'pressureScore'],df.at[0, 'staticScore'],df.at[0, 'kinematicsScore'],df.at[0, 'tiltScore']]) == df.at[0, 'pressureScore']):
+                msg = 'Jouons au jeu submarine!'
+                rospy.loginfo(msg)
+                self.pubSayMsg.publish(msg)
+            if (min([df.at[0, 'pressureScore'],df.at[0, 'staticScore'],df.at[0, 'kinematicsScore'],df.at[0, 'tiltScore']]) == df.at[0, 'staticScore']):
+                msg = 'Jouons au jeu chemist!'
+                rospy.loginfo(msg)
+                self.pubSayMsg.publish(msg)
+            if (min([df.at[0, 'pressureScore'],df.at[0, 'staticScore'],df.at[0, 'kinematicsScore'],df.at[0, 'tiltScore']]) == df.at[0, 'kinematicsScore']):
+                msg = 'Jouons au jeu pursuit!'
+                rospy.loginfo(msg)
+                self.pubSayMsg.publish(msg)
+            if (min([df.at[0, 'pressureScore'],df.at[0, 'staticScore'],df.at[0, 'kinematicsScore'],df.at[0, 'tiltScore']]) == df.at[0, 'tiltScore']):
+                msg = 'Jouons au jeu copter!'
+                rospy.loginfo(msg)
+                self.pubSayMsg.publish(msg)
+            else:
+                msg = 'Jouons au jeu apprentice!'
+                rospy.loginfo(msg)
+                self.pubSayMsg.publish(msg)
+            msg = 'goToActivity'
             rospy.loginfo(msg)
-            self.pubBehMsg.publish(msg)
-            msg = 'goToGoodbye'
-            rospy.loginfo(msg)
-            self.pubFSMMsg.publish(msg)
+            self.pubFSMMsg.publish(msg)  
+        # if ACTIVITY
+        #   if the score in the current game is above 50 --> congratulations, move to GOODBYE state
+        #   else --> encouragement and try again
         else:
-            msg = 'courage'
-            rospy.loginfo(msg)
-            self.pubBehMsg.publish(msg)
+            if (df.at[0, 'score'] > 50 ):
+                msg = 'bravo'
+                rospy.loginfo(msg)
+                self.pubBehMsg.publish(msg)
+                msg = 'goToGoodbye'
+                rospy.loginfo(msg)
+                self.pubFSMMsg.publish(msg)
+            else:
+                msg = 'courage'
+                rospy.loginfo(msg)
+                self.pubBehMsg.publish(msg)
 
 
 #########################################################################
